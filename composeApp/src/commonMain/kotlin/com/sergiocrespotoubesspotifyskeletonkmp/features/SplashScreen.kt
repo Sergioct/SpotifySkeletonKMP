a99e2c4d4c28bab9e3d9b498bfe7340e9a5ddddc
SergioCT <sergio.crespo@abancagen.com>
2025-06-03 22:13:35 +0200

    feat(Theme&UI): Add Spotify theme and UI components
    This commit introduces the initial Spotify theme and UI components, including:
    
    - **Theme:**
        - `SpotifyTheme`: Base theme composable.
        - `SpotifyThemeColors`: Defines light and dark color schemes.
        - `SpotifyColor`: Contains specific color values.
        - `SpotifyThemeConfig`: Configures MaterialTheme with Spotify colors.
        - `SpotifyDimen`: Provides dimension resources.
        - `SpotifyTextStyle`: Defines various text styles (title, subtitle, big, medium, small).
        - Resource files: `colors.xml`, `dimens.xml`, `strings.xml`, `themes.xml`.
    - **UI Components:**
        - `SpotifyImage`: Composable for displaying static images.
        - `SpotifyIconImage`: Composable for displaying vector icons.
        - `SpotifyAsyncImage`: Composable for loading images asynchronously (currently commented out).
    - **Features:**
        - `SplashScreen`: Initial splash screen composable with preview.
    - **Resources:**
        - Added placeholder and splash screen images.
    
    The main `App` composable has been updated to use the new `SpotifyTheme` and display the `SplashScreen`.

---
+++ composeApp/src/commonMain/kotlin/com/sergiocrespotoubesspotifyskeletonkmp/features/SplashScreen.kt
@@ -0,0 +1,109 @@
+package com.sergiocrespotoubesspotifyskeletonkmp.features
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.key.Key.Companion.R
+import com.sergiocrespotoubesspotifyskeletonkmp.theme.SpotifyDimen
+import com.sergiocrespotoubesspotifyskeletonkmp.theme.SpotifyTheme
+import com.sergiocrespotoubesspotifyskeletonkmp.ui.image.SpotifyImage
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+fun SplashScreen(
+    //splashViewModel: SplashViewModel = hiltViewModel(),
+    navigateToArtistSearch: () -> Unit,
+) {
+    //val context = LocalContext.current
+    //val state = splashViewModel.state.collectAsState().value
+    Design(
+        /*context,
+        state = state,
+        onRetryClick = { splashViewModel.onRetryClick() },*/
+    )
+    /*ReadEvents(
+        splashViewModel = splashViewModel,
+        navigateToArtistSearch = navigateToArtistSearch,
+    )*/
+}
+
+@Composable
+private fun Design(
+    //context: Context,
+    //state: SplashViewModel.State,
+    //onRetryClick: () -> Unit,
+) {
+    Box(modifier = Modifier.fillMaxSize()) {
+        Column(
+            modifier =
+                Modifier
+                    .align(Alignment.Center)
+                    .padding(horizontal = SpotifyDimen.spaceBig()),
+        ) {
+            SpotifyImage(
+                painter = painterResource(R.drawable.splash_spotify),
+                contentDescription = //stringResource(R.string.splash_name),
+            )
+            /*SpotifyTextTitleBold(
+                modifier =
+                    Modifier
+                        .align(Alignment.CenterHorizontally)
+                        .padding(top = SpotifyDimen.spaceMedium()),
+                text = getString(context, R.string.app_name),
+            )
+            if (state == SplashViewModel.State.Error) {
+                SpotifyButton(
+                    modifier =
+                        Modifier
+                            .align(Alignment.CenterHorizontally)
+                            .padding(top = SpotifyDimen.spaceBig())
+                            .wrapContentSize(),
+                    text = stringResource(R.string.splash_retry_button),
+                    onClick = { onRetryClick() },
+                )
+            }*/
+        }
+    }
+}
+
+/*@Composable
+fun ReadEvents(
+    splashViewModel: SplashViewModel,
+    navigateToArtistSearch: () -> Unit,
+) {
+    val context = LocalContext.current
+    LaunchedEffect(Unit) {
+        splashViewModel.event.collect { event ->
+            when (event) {
+                is SplashViewModel.Event.NavigateToArtistSearch -> {
+                    navigateToArtistSearch()
+                }
+                SplashViewModel.Event.ShowError ->
+                    showToastError(context)
+            }
+        }
+    }
+}
+
+private fun showToastError(context: Context) {
+    Toast.makeText(
+        context,
+        context.getString(R.string.splash_error_message),
+        Toast.LENGTH_LONG,
+    ).show()
+}*/
+
+@Preview
+@Composable
+fun SplashScreenPreview() {
+    SpotifyTheme {
+        SplashScreen(
+            navigateToArtistSearch = {},
+        )
+    }
+}
---
+++ composeApp/src/commonMain/kotlin/com/sergiocrespotoubesspotifyskeletonkmp/ui/image/SpotifyAsyncImage.kt
@@ -0,0 +1,28 @@
+package com.sergiocrespotoubesspotifyskeletonkmp.ui.image
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+
+@Composable
+fun SpotifyAsyncImage(
+    modifier: Modifier = Modifier,
+    model: Any?,
+//    placeholder: Painter? = null,
+    contentDescription: String,
+    contentScale: ContentScale = ContentScale.Fit,
+    error: Painter? = null,
+    color: Color? = null,
+) {
+    /*AsyncImage(
+        modifier = modifier,
+        model = model,
+        placeholder = placeholder,
+        contentDescription = contentDescription,
+        contentScale = contentScale,
+        error = error,
+        colorFilter = color?.let { ColorFilter.tint(it) },
+    )*/
+}
